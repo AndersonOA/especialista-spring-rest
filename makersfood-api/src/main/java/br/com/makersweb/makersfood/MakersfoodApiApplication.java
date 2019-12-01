0f53478c05dbd9027912d7452621ba9680abba9f
Anderson O. Aristides <anderson.aristides@fh.com.br>
2019-12-01 14:44:03 -0300

    Feat: Commit initial project

---
+++ makersfood-api/src/main/java/br/com/makersweb/makersfood/MakersfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.makersweb.makersfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakersfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakersfoodApiApplication.class, args);
+	}
+
+}
