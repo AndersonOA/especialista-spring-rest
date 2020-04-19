1f017fab01fee7434ecaf191da1e623602ae99d9
Anderson O. Aristides <anderson.aristides@fh.com.br>
2020-04-19 15:10:53 -0300

    Feat: Init Commit

--- makersfood-api/src/test/java/br/com/makersweb/makersfood/MakersfoodApiApplicationTests.java
+++ makersfood-api/src/test/java/br/com/makersweb/makersfood/MakersfoodApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MakersfoodApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
